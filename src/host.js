c662d1329db7680fe13510c922a1a523e2346c0a
fendywillyam <fendy.willyam@moonlay.com>
2016-12-07 11:19:43 +0700

    Update host.js

--- src/host.js
+++ src/host.js
@@ -1,13 +1,22 @@
 module.exports = { 
     //DEV
-    core: "http://bateeq-core-api-dev.mybluemix.net/v1/core",
-    auth: "http://bateeq-auth-api-dev.mybluemix.net",
-    inventory: "http://bateeq-inventory-api-dev.mybluemix.net/v1/inventory",
-    merchandiser:"http://bateeq-inventory-api-dev.mybluemix.net/v1/merchandiser",
-    master: "http://bateeq-pos-api-dev.mybluemix.net/v1/master",
-    sales: "http://bateeq-pos-api-dev.mybluemix.net/v1/sales",
-    store: "http://bateeq-pos-api-dev.mybluemix.net/v1/store"
+    //core: "http://bateeq-core-api-dev.mybluemix.net/v1/core",
+    //auth: "http://bateeq-auth-api-dev.mybluemix.net",
+    //inventory: "http://bateeq-inventory-api-dev.mybluemix.net/v1/inventory",
+    //merchandiser:"http://bateeq-inventory-api-dev.mybluemix.net/v1/merchandiser",
+    //master: "http://bateeq-pos-api-dev.mybluemix.net/v1/master",
+    //sales: "http://bateeq-pos-api-dev.mybluemix.net/v1/sales",
+    //store: "http://bateeq-pos-api-dev.mybluemix.net/v1/store"
    
+    //UAT
+    core: "http://bateeq-core-api-uat.mybluemix.net/v1/core",
+    auth: "http://bateeq-auth-api-uat.mybluemix.net",
+    inventory: "http://bateeq-inventory-api-uat.mybluemix.net/v1/inventory",
+    merchandiser:"http://bateeq-inventory-api-uat.mybluemix.net/v1/merchandiser",
+    master: "http://bateeq-pos-api-uat.mybluemix.net/v1/master",
+    sales: "http://bateeq-pos-api-uat.mybluemix.net/v1/sales",
+    store: "http://bateeq-pos-api-uat.mybluemix.net/v1/store"
+    
     //Local
     // , auth: "http://localhost:9050",
     // inventory: "http://localhost:9070/v1/inventory",
