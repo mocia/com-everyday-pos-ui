e3f7bf87af8fddf1da23d2739202ea7ac7e63717
novalinahtb <38346495+novalinahtb@users.noreply.github.com>
2018-04-19 17:13:22 +0700

    update

--- src/main.js
+++ src/main.js
@@ -18,11 +18,7 @@ export async function configure(aurelia) {
     .standardConfiguration()
     .feature('components')
     .plugin("aurelia-api", config => {
-
-     // var auth = "https://bateeq-auth-api-dev.mybluemix.net/v1/";
-     // var core = "https://bateeq-core-api-dev.mybluemix.net/v1/";
-      //var pos = "https://bateeq-pos-api-dev.mybluemix.net/v1/";
-
+    
        var auth = "https://bateeq-auth-api-uat.mybluemix.net/v1/";
        var core = "https://bateeq-core-api-uat.mybluemix.net/v1/";
        var pos = "https://bateeq-pos-api-uat.mybluemix.net/v1/";
