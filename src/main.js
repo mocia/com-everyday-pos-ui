a48159ebbdd503cec1b95dc25eee4773b6870da0
edo-moonlay <31334028+edo-moonlay@users.noreply.github.com>
2018-09-19 22:14:26 +0700

    update to uat

--- src/main.js
+++ src/main.js
@@ -18,10 +18,10 @@ export async function configure(aurelia) {
     .standardConfiguration()
     .feature('components')
     .plugin("aurelia-api", config => {
-      var auth = "https://bateeq-auth-api-dev.azurewebsites.net/v1/";
-      var core = "https://bateeq-core-api-dev.azurewebsites.net/v1/";
-      var pos = "https://bateeq-pos-api-dev.azurewebsites.net/v1/";
-      var inventory = "https://bateeq-inventory-api-dev.azurewebsites.net/v1/inventory/";
+      var auth = "https://bateeq-auth-api-uat.azurewebsites.net/v1/";
+      var core = "https://bateeq-core-api-uat.azurewebsites.net/v1/";
+      var pos = "https://bateeq-pos-api-uat.azurewebsites.net/v1/";
+      var inventory = "https://bateeq-inventory-api-uat.azurewebsites.net/v1/inventory/";
 
       config.registerEndpoint('auth', auth);
       config.registerEndpoint('core', core);
