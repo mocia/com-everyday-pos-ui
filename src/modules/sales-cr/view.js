d0e022e6386e28884ff4c9516ab5a372a6954be4
Andi Budi <andibateeq@gmail.com>
2020-09-22 11:23:01 +0700

    Add field accountBateeqshop

--- src/modules/sales-cr/view.js
+++ src/modules/sales-cr/view.js
@@ -291,6 +291,15 @@ export class View {
         // this.printStruk += "            <div class='col-xs-9'> " + this.data.dateString + " </div>";  
         this.printStruk += "        </td>";
         this.printStruk += "    </tr>";
+
+        this.printStruk += "    <tr>";
+        this.printStruk += "        <td colspan='3' class='text-left'>";
+        this.printStruk += " Account Bateeqshop : " + this.data.accountBateeqshop;
+        // this.printStruk += "            <div class='col-xs-3'> Nomor </div>"; 
+        // this.printStruk += "            <div class='col-xs-9'> " + this.data.code + " </div>";  
+        this.printStruk += "        </td>";
+        this.printStruk += "    </tr>";
+
         this.printStruk += "    <tr>";
         this.printStruk += "        <td colspan='3' class='text-left'> =============================== </td>";
         this.printStruk += "    </tr>";
