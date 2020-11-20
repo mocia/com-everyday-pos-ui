add7a3eadda54f3748544367bd0474ed888b59fb
yokana777 <kawaiirockmansick@gmail.com>
2020-11-20 07:44:22 +0700

    voidreport

--- src/modules/report-void-sales/list.js
+++ src/modules/report-void-sales/list.js
@@ -7,7 +7,7 @@ import moment from 'moment';
 @inject(Router, Service, BindingEngine, LocalStorage)
 export class List {
 
-    shifts = ["Semua", "1", "2", "3", "4", "5"];
+    shifts = ["Semua", "1", "2"];
 
     constructor(router, service, bindingEngine, localStorage) {
         this.router = router;
@@ -47,6 +47,7 @@ export class List {
         this.data.filter.shift = 0;
         this.data.filter.storeId = this.localStorage.store._id;
         this.data.filter.store = this.localStorage.store;
+        this.data.filter.storeCode = this.localStorage.store.code;
         // this.data.filter.storeId = this.session.store._id;
         // this.data.filter.store = this.session.store;
         // this.bindingEngine.propertyObserver(this.data.filter, "storeId").subscribe((newValue, oldValue) => { 
@@ -55,50 +56,63 @@ export class List {
 
     filter() {
         this.error = { filter: {}, results: [] };
-        var datefrom = new Date(this.data.filter.dateFrom);
-        var dateto = new Date(this.data.filter.dateTo);
+        var datefrom = moment(this.data.filter.dateFrom).startOf('day');
+        var dateto = moment(this.data.filter.dateTo).endOf('day');
+        var storeCode = this.data.filter.storeCode;
+        var shift = this.data.filter.shift;
         if (dateto < datefrom)
             this.error.filter.dateTo = "Tanggal From Harus Lebih Besar Dari To";
         else {
             var getData = [];
-            for (var d = datefrom; d <= dateto; d.setDate(d.getDate() + 1)) {
-                var date = new Date(d);
-                var from = moment(d).startOf('day');
-                var to = moment(d).endOf('day');
-                getData.push(this.service.getAllSalesByFilter(this.data.filter.storeId, from.format('YYYY-MM-DD'), to.format('YYYY-MM-DD'), this.data.filter.shift));
+            // for (var d = datefrom; d <= dateto; d.setDate(d.getDate() + 1)) {
+                // var date = new Date(d);
+                // var from = moment(d).startOf('day');
+                // var to = moment(d).endOf('day');
 
-            }
+                let args = {
+                    dateFrom : datefrom.format('YYYY-MM-DD'),
+                    dateTo: dateto.format('YYYY-MM-DD'),
+                    storeCode: storeCode,
+                    shift: shift 
+                }
+                
+                getData.push(this.service.getAllSalesByFilter(args));
+            //}
             Promise.all(getData)
                 .then(salesPerDays => {
                     this.data.results = [];
-                    for (var salesPerDay of salesPerDays) {
-                        if (salesPerDay.length != 0) {
-                            var totalSubTotal = 0;
-
-                            var tanggalRowSpan = 0;
-                            var result = {};
-                            result.items = [];
-                            for (var data of salesPerDay) {
+                    for (var data of salesPerDays) {
+                        if (data.length != 0) {
+                            for(var item of data){
+                                var totalSubTotal = 0;
+                                var tanggalRowSpan = 0;
+                                var result = {};
+                                //result.items = [];
+                                //for (var data of salesPerDay) {
                                 var itemRowSpan = 0;
-                                var itemData = {};
-                                itemData.details = [];
-                                result.tanggal = new Date(data._updatedDate);
-                                itemData.isVoid = data.isVoid;
-                                itemData.nomorPembayaran = data.code;
-                                itemData.Toko = data.store.name;
-                                itemData.grandTotal = data.grandTotal;
-                                itemData._createdBy = data._createdBy;
-                                itemData.shift = data.shift;
-                                itemData._updatedBy = data._updatedBy;
-                                itemData.itemRowSpan = itemRowSpan;
-                                totalSubTotal += parseInt(itemData.subTotal);
+                                //var itemData = {};
+                                //itemData.details = [];
+                                result.tanggal = new Date(item._LastModifiedUtc);
+                                result.isVoid = item.isVoid;
+                                result.nomorPembayaran = item.code;
+                                result.Toko = item.storeName
+                                result.barcode = item.ItemCode;
+                                result.itemName = item.ItemName;
+                                result.quantity = item.Quantity;
+                                //result.grandTotal = data.grandTotal;
+                                result.subTotal = item.TotalPrice
+                                result.shift = item.shift;
+                                result._createdBy = item._createdBy;
+                                result._updatedBy = item._LastModifiedBy;
+                                result.itemRowSpan = itemRowSpan;
+                                totalSubTotal += parseInt(item.subTotal);
+                                //result.items.push(itemData);
+                                //}
+                                //result.totalSubTotal = totalSubTotal;
 
-                                result.items.push(itemData);
-                            }
-                            result.totalSubTotal = totalSubTotal;
-
-                            result.tanggalRowSpan = tanggalRowSpan;
-                            this.data.results.push(result);
+                                result.tanggalRowSpan = tanggalRowSpan;
+                                this.data.results.push(result);
+                            }   
                         }
                     }
                     this.generateReportHTML();
@@ -124,7 +138,7 @@ export class List {
     setShift(e) {
         var _shift = (e ? (e.srcElement.value ? e.srcElement.value : e.detail) : this.shift);
         if (_shift.toLowerCase() == 'semua') {
-            this.data.filter.shift = 0;
+            this.data.filter.shift = "";
         } else {
             this.data.filter.shift = parseInt(_shift);
         }
@@ -158,8 +172,12 @@ export class List {
         this.reportHTML += "            <tr style='background-color:#282828; color:#ffffff;'>";
         this.reportHTML += "                <th>Tanggal</th>";
         this.reportHTML += "                <th>Nomor Transaksi</th>";
+        this.reportHTML += "                <th>Barcode</th>";
+        this.reportHTML += "                <th>Nama Barang</th>";
+        this.reportHTML += "                <th>Kuantitas</th>";
+        this.reportHTML += "                <th>Total Harga</th>";
         this.reportHTML += "                <th>Toko</th>";
-        this.reportHTML += "                <th>Grand Total</th>";
+        //this.reportHTML += "                <th>Grand Total</th>";
         this.reportHTML += "                <th>Kasir</th>";
         this.reportHTML += "                <th>Shift</th>";
         this.reportHTML += "                <th>Di Void oleh</th>";
@@ -171,35 +189,40 @@ export class List {
         for (var data of this.data.results) {
             var isTanggalRowSpan = false;
 
-            for (var item of data.items) {
+            //for (var item of data.items) {
                 var isItemRowSpan = false;
-                if (item.isVoid) {
+                //if (item.isVoid) {
                     this.reportHTML += "        <tr>";
 
-                    this.reportHTML += "        <td width='300px' rowspan='" + data.tanggalRowSpan + "'>" + data.tanggal.getDate() + " " + months[data.tanggal.getMonth()] + " " + data.tanggal.getFullYear() + "</td>";
+                    this.reportHTML += "            <td width='300px' rowspan='" + data.tanggalRowSpan + "'>" + data.tanggal.getDate() + " " + months[data.tanggal.getMonth()] + " " + data.tanggal.getFullYear() + "</td>";
 
-                    this.reportHTML += "            <td rowspan='" + data.tanggalRowSpan + "'>" + item.nomorPembayaran + "</td>";
+                    this.reportHTML += "            <td rowspan='" + data.tanggalRowSpan + "'>" + data.nomorPembayaran + "</td>";
 
-                    this.reportHTML += "            <td rowspan='" + data.tanggalRowSpan + "'>" + item.Toko + "</td>";
+                    this.reportHTML += "            <td rowspan='" + data.tanggalRowSpan + "'>" + data.barcode + "</td>";
 
-                    this.reportHTML += "            <td rowspan='" + data.tanggalRowSpan + "'>" + item.grandTotal + "</td>";
+                    this.reportHTML += "            <td rowspan='" + data.tanggalRowSpan + "'>" + data.itemName + "</td>";
 
+                    this.reportHTML += "            <td rowspan='" + data.tanggalRowSpan + "'>" + data.quantity + "</td>";
 
-                    this.reportHTML += "            <td rowspan='" + data.tanggalRowSpan + "'>" + item._createdBy + "</td>";
+                    this.reportHTML += "            <td rowspan='" + data.tanggalRowSpan + "'>" + data.subTotal + "</td>";
 
-                    this.reportHTML += "            <td rowspan='" + data.tanggalRowSpan + "'>" + item.shift + "</td>";
+                    this.reportHTML += "            <td rowspan='" + data.tanggalRowSpan + "'>" + data.Toko + "</td>";
 
-                    this.reportHTML += "            <td rowspan='" + data.tanggalRowSpan + "'>" + item._updatedBy + "</td>";
+                    this.reportHTML += "            <td rowspan='" + data.tanggalRowSpan + "'>" + data._createdBy + "</td>";
+
+                    this.reportHTML += "            <td rowspan='" + data.tanggalRowSpan + "'>" + data.shift + "</td>";
+
+                    this.reportHTML += "            <td rowspan='" + data.tanggalRowSpan + "'>" + data._updatedBy + "</td>";
 
                     this.reportHTML += "        </tr>";
-                }
+                //}
                 isTanggalRowSpan = true;
                 isItemRowSpan = true;
-
-
             }
+        this.reportHTML += "        </tbody>";
+        this.reportHTML += "    </table>";
         }
-
+        
 
     }
-}
+//}
